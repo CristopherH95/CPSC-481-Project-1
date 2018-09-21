1e46526c073c3f4978397a7c77d18f9d87dca7b6
Cristopher Hernandez <DoughboysWWI@gmail.com>
2018-09-21 15:03:25 -0700

    Refactor 'start' parameter to be more clear

--- project_1.lisp
+++ project_1.lisp
@@ -23,11 +23,11 @@
         (list (- row 1) (- col 2))))
 
 ;;; Recursive path-finding
-(defun find-path (start)
+(defun find-path (curr-path)
     "Recursively walks the tree of giraffe moves on a 7x7 board to get a path covering the whole board.
     The board is 0-indexed with (0 0) representing the upper-left, and (6 6) representing the bottom right.
         Expects: 
-            The argument start should be a list whose elements are of the form:
+            The argument curr-path should be a list whose elements are of the form:
             (Y . X)
             This argument represents the current path followed on the board.
             The first element is assumed to be the current position.
@@ -40,18 +40,18 @@
             Nil represents no more valid moves found."
     (cond
         ;; parameter is not a list, return nil
-        ((not (listp start)) (return-from find-path nil))
+        ((not (listp curr-path)) (return-from find-path nil))
         ;; parameter has enough moves to cover the board
-        ((>= (length start) 49) (return-from find-path (reverse start)))
+        ((>= (length curr-path) 49) (return-from find-path (reverse curr-path)))
         ;; generate more moves from current position
-        (T (let ((pos-moves (gen-moves (nth 0 (nth 0 start)) (nth 1 (nth 0 start)))))
+        (T (let ((pos-moves (gen-moves (nth 0 (nth 0 curr-path)) (nth 1 (nth 0 curr-path)))))
                 (loop 
                     for move in pos-moves
                     do (if (and 
-                       (not (member move start :test 'equal)) ; No repeat moves 
+                       (not (member move curr-path :test 'equal)) ; No repeat moves 
                        (not (or (< (nth 0 move) 0) (< (nth 1 move) 0) ; No moves off board 
                             (> (nth 0 move) 6) (> (nth 1 move) 6)))) 
-                                (return-from find-path (find-path (cons move start)))
+                                (return-from find-path (find-path (cons move curr-path)))
                         nil)
                 )
             )
