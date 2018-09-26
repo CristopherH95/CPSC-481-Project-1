d2b83268a6193b3c5a7722c16e7c1990e56b79e4
Cristopher Hernandez <DoughboysWWI@gmail.com>
2018-09-25 21:31:19 -0700

    Use local variable for new path generation

--- project_1.lisp
+++ project_1.lisp
@@ -52,17 +52,22 @@
         ;; parameter is not a list, return nil
         ((not (listp curr-path)) (return-from find-path nil))
         ;; parameter has enough moves to cover the board
-        ((>= (length curr-path) 49) (return-from find-path (reverse curr-path)))
+        ((>= (length curr-path) 49) (return-from find-path curr-path))
         ;; generate more moves from current position
-        (T (let ((pos-moves (gen-moves (nth 0 (nth 0 curr-path)) (nth 1 (nth 0 curr-path)))))
+        (T (let ((pos-moves (gen-moves (nth 0 (nth 0 curr-path)) (nth 1 (nth 0 curr-path))))
+                (new-path nil))
                 (loop 
                     for move in pos-moves
                     do (if (and 
-                       (not (member move curr-path :test 'equal)) ; No repeat moves 
+                       (not (or (member move curr-path :test 'equal) (member move new-path :test 'equal))) ; No repeat moves 
                        (not (or (< (nth 0 move) 0) (< (nth 1 move) 0) ; No moves off board 
-                            (> (nth 0 move) 6) (> (nth 1 move) 6)))) 
-                                (return-from find-path (find-path (cons move curr-path)))
+                            (> (nth 0 move) 6) (> (nth 1 move) 6))))
+                                  (if new-path 
+                                    (setf new-path (find-path (cons move new-path)))
+                                  (setf new-path (find-path (cons move curr-path))))     
                         nil)
+                    finally 
+                        (return-from find-path new-path)
                 )
             )
         )
